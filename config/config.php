dfec15b6269ae9ac90cf2c03b093a873bdd2df81
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-10-21 16:38:08 -0600

    Creating a users register system.

--- config/config.php
+++ config/config.php
@@ -7,9 +7,9 @@
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    if ($conn) {
-        echo "worked successfully";
-    } else {
-        echo "error in db connection";
-    }
+    // if ($conn) {
+    //     echo "worked successfully";
+    // } else {
+    //     echo "error in db connection";
+    // }
 ?>
