e2551855c883f55ca80629c0cb66b52e54e87328
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-11-18 20:54:11 -0600

    Showing the products belongs to each category.

--- categories/index.php
+++ categories/index.php
@@ -2,12 +2,10 @@
 <?php require "../config/config.php"; ?>
 
 <?php
-
     $select = $conn->query("SELECT * FROM categories");
     $select->execute();
 
     $categories = $select->fetchAll(PDO::FETCH_OBJ);
-
 ?>
 
         <div class="row mt-5">
@@ -20,7 +18,7 @@
                             <div class="d-flex flex-row my-2">
                                 <div class="text-muted"><?php echo $category->description; ?></div>
                             </div> 
-                            <a href="#" class="btn btn-primary w-100 rounded my-2">Discover Products</a>      
+                            <a href="<?php echo APPURL; ?>/categories/single-category.php?id=<?php echo $category->id;?>" class="btn btn-primary w-100 rounded my-2">Discover Products</a>      
                         </div>
                     </div>
                 </div>
