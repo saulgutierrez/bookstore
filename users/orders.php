4a2d3cd334e01fc5557a7773cc757995c353a5b7
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-12-26 22:15:45 -0600

    Show a message if there are no orders making for any user

--- users/orders.php
+++ users/orders.php
@@ -21,38 +21,41 @@
   $orders = $select->fetchAll(PDO::FETCH_OBJ);
 ?>
 
-      <div class="row mt-5">
+      <div class="row mt-5" style="margin-bottom: 220px;">
         <div class="col">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title mb-4 d-inline">Orders</h5>
-             <!-- <a  href="<?php //echo ADMINURL; ?>/admins/create-admins.php" class="btn btn-primary mb-4 text-center float-right">Create Admins</a> -->
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">username</th>
-                    <th scope="col">email</th>
-                    <th scope="col">first name</th>
-                    <th scope="col">last name</th>
-                    <th scope="col">status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php foreach($orders as $order): ?>
-                    <tr>
-                      <th scope="row"><?php echo $order->id; ?></th>
-                      <td><?php echo $order->username; ?></td>
-                      <td><?php echo $order->email; ?></td>
-                      <td><?php echo $order->fname; ?></td>
-                      <td><?php echo $order->lname; ?></td>
-                      <td><?php echo 'completed'; ?></td>
-                    </tr>
-                  <?php endforeach; ?>
-                </tbody>
-              </table> 
-            </div>
-          </div>
+            <?php if (count($orders) > 0): ?>
+                <div class="card">
+                    <div class="card-body">
+                    <h5 class="card-title mb-4 d-inline">Orders</h5>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">username</th>
+                            <th scope="col">email</th>
+                            <th scope="col">first name</th>
+                            <th scope="col">last name</th>
+                            <th scope="col">status</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach($orders as $order): ?>
+                            <tr>
+                            <th scope="row"><?php echo $order->id; ?></th>
+                            <td><?php echo $order->username; ?></td>
+                            <td><?php echo $order->email; ?></td>
+                            <td><?php echo $order->fname; ?></td>
+                            <td><?php echo $order->lname; ?></td>
+                            <td><?php echo 'completed'; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table> 
+                    </div>
+                </div>
+            <?php else: ?>
+                <div class="alert alert-success text-white bg-success">There are no orders for now</div>
+            <?php endif; ?>
         </div>
       </div>
 
