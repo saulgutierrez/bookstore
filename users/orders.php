3a26419043a594f308e9a291c913a89b8b6bde0c
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-12-26 21:59:01 -0600

    We can see a list of all orders or records of products purchased for every user.

---
+++ users/orders.php
@@ -0,0 +1,60 @@
+<?php require "../includes/header.php"; ?>
+<?php require "../config/config.php"; ?>
+
+<?php
+  if (!isset($_SESSION['username'])) {
+        header("location: ".APPURL."");
+  }
+
+  if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    // Avoid to access the orders of another user
+    if ($id != $_SESSION['user_id']) {
+        header("location: ".APPURL."");
+    }
+  }
+  
+  $select = $conn->query("SELECT * FROM orders WHERE user_id ='$_SESSION[user_id]'");
+  $select->execute();
+
+  $orders = $select->fetchAll(PDO::FETCH_OBJ);
+?>
+
+      <div class="row mt-5">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-4 d-inline">Orders</h5>
+             <!-- <a  href="<?php //echo ADMINURL; ?>/admins/create-admins.php" class="btn btn-primary mb-4 text-center float-right">Create Admins</a> -->
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">username</th>
+                    <th scope="col">email</th>
+                    <th scope="col">first name</th>
+                    <th scope="col">last name</th>
+                    <th scope="col">status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach($orders as $order): ?>
+                    <tr>
+                      <th scope="row"><?php echo $order->id; ?></th>
+                      <td><?php echo $order->username; ?></td>
+                      <td><?php echo $order->email; ?></td>
+                      <td><?php echo $order->fname; ?></td>
+                      <td><?php echo $order->lname; ?></td>
+                      <td><?php echo 'completed'; ?></td>
+                    </tr>
+                  <?php endforeach; ?>
+                </tbody>
+              </table> 
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+<?php require "../includes/footer.php"; ?>
